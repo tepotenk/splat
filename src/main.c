fa67c18f27ad704ba10c78d0ce25625a3c1e36ab
Robert Knutsson <zybreak@gmail.com>
2010-06-01 22:03:08 +0200

    added some sdl stuff

--- src/main.c
+++ src/main.c
@@ -4,5 +4,14 @@
 
 int main(int argc, char* argv[]) {
 	printf("%s version %s\n",PACKAGE_NAME, PACKAGE_VERSION);
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		char *error = SDL_GetError();
+		printf("Error: %s\n",error);
+		return -1;
+	}
+
+	SDL_Quit();
+
 	return 0;
 }
